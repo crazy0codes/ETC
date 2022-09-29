b143703496708ae957662a8e8fe2ab836d64c3a3
Madhan <107941826+crazy0codes@users.noreply.github.com>
2022-09-29 09:22:22 +0530

    Submit.php

---
+++ submit.php
@@ -0,0 +1,21 @@
+<?php
+//Client
+$name = $_POST["name"];
+$psw  = $_POST['password'];
+//
+$host = "root";
+$server = "127.0.0.1";
+$pass = "";
+$db = "ETC";
+//
+$conn = mysqli_connect($server,$host,$pass,$db); //server details
+//
+$querry2 = "SELECT * FROM STUDENTS
+            where name = '$name' and pass = '$psw' ";
+$results = mysqli_query($conn,$querry2);
+$details = mysqli_fetch_assoc($results);
+if($details['name']==null){
+    echo $_SERVER("/xampp/htdocs/dashboard/Madhan_ETC/index.html");
+}
+mysqli_close($conn);//CONNCETION Closed
+?>
